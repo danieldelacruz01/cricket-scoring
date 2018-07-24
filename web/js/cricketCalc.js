96121dea80d3b28298d37f9da4d8fceafd929700
Rongji Wang(Kevin) <rongjiwang0312@gmail.com>
2018-07-24 17:11:21 +1200

    fix NaN bug

--- web/js/cricketCalc.js
+++ web/js/cricketCalc.js
@@ -18,6 +18,9 @@ function enter(key) {
 */
 function backspace() {
     var previousBall = scorecard.charAt(scorecard.length - 1);
+    if(previousBall == "."){
+        previousBall = 0
+    }
     score = score - previousBall;
     scorecard = scorecard.substring(0, scorecard.length - 1);
     document.getElementById('scorecard').innerHTML = scorecard;
